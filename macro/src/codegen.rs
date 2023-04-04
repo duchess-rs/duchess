eacb16932c310b21e20f2b3bec843bf0c95e1c12
Niko Matsakis <niko@alum.mit.edu>
2023-04-04 04:17:21 -0400

    cleanup wildcard handling

--- macro/src/codegen.rs
+++ macro/src/codegen.rs
@@ -99,7 +99,21 @@ struct Signature {
     span: Span,
     generics: Vec<Ident>,
     where_bounds: Vec<TokenStream>,
+    capture_generics: bool,
 }
+
+/// We translate Java wildcards to Rust generics, but there are
+/// limits to what we can do with this technique. For example,
+/// an input `ArrayList<ArrayList<?>>` has no Rust equivalent,
+/// it would be something like `ArrayList<exists<T> ArrayList<T>>`,
+/// if we had `exists`. Similarly returning a type like
+/// `-> ArrayList<? extends Foo>` isn't possible today, though we
+/// could conceivably handle it via some kind of fresh struct or
+/// `impl Trait` return value.
+///
+/// When we encounter cases like this, we return
+/// `Err(UnsupportedWildcard)`.
+struct UnsupportedWildcard;
 
 impl Signature {
     pub fn new(span: Span, generics: &[Id]) -> Self {
@@ -107,6 +121,7 @@ impl Signature {
             span,
             generics: vec![],
             where_bounds: vec![],
+            capture_generics: true,
         };
         for generic in generics {
             let ident = this.java_type_parameter_ident(generic);
@@ -115,75 +130,138 @@ impl Signature {
         this
     }
 
-    fn fresh_generic(&mut self) -> Ident {
-        let mut i = self.generics.len();
-        let ident = Ident::new(&format!("P{}", i), self.span);
-        self.generics.push(ident.clone());
-        ident
+    /// Set the `capture_generics` field to false while `op` executes,
+    /// then restore its value.
+    fn forbid_capture<R>(&mut self, op: impl FnOnce(&mut Self) -> R) -> R {
+        let v = std::mem::replace(&mut self.capture_generics, false);
+        let r = op(self);
+        self.capture_generics = v;
+        r
     }
 
+    /// Generates a fresh generic type and adds it to `self.generics`.
+    ///
+    /// Used to manage Java wildcards. A type like `ArrayList<?>` gets
+    /// translated to a Rust type like `ArrayList<Pi>` for some fresh `Pi`.
+    ///
+    /// See also `Self::push_where_bound`.
+    fn fresh_generic(&mut self) -> Result<Ident, UnsupportedWildcard> {
+        if !self.capture_generics {
+            Err(UnsupportedWildcard)
+        } else {
+            let mut i = self.generics.len();
+            let ident = Ident::new(&format!("P{}", i), self.span);
+            self.generics.push(ident.clone());
+            Ok(ident)
+        }
+    }
+
+    /// Push a where bound into the list of where clauses that will be
+    /// emitted later. Used to manage Java wildcards. A type like
+    /// `ArrayList<? extends Foo>` becomes `ArrayList<X>` with a bound
+    /// `X: Upcast<Foo>`.
+    ///
+    /// See also `Self::fresh_generic`.
     fn push_where_bound(&mut self, t: TokenStream) {
         self.where_bounds.push(t);
     }
 
-    fn java_type(&mut self, ty: &Type) -> TokenStream {
+    /// Returnss an appropriate `impl type` for a funtion that
+    /// takes `ty` as input. Assumes objects are nullable.
+    fn impl_input(&mut self, ty: &Type) -> Result<TokenStream, UnsupportedWildcard> {
+        match ty {
+            Type::Ref(ty) => {
+                let t = self.java_ref_ty(ty)?;
+                Ok(quote_spanned!(self.span => impl IntoJava<$t>))
+            }
+            Type::Scalar(ty) => {
+                let t = self.java_scalar_ty(ty);
+                Ok(quote_spanned!(self.span => impl IntoScalar<$t>))
+            }
+        }
+    }
+
+    /// Returnss an appropriate `impl type` for a funtion that
+    /// returns `ty`. Assumes objects are nullable.
+    fn impl_output(&mut self, ty: &Type) -> Result<TokenStream, UnsupportedWildcard> {
+        self.forbid_capture(|this| match ty {
+            Type::Ref(ty) => {
+                let t = this.java_ref_ty(ty)?;
+                Ok(quote_spanned!(this.span => impl IntoOptLocal<$t>))
+            }
+            Type::Scalar(ty) => {
+                let t = this.java_scalar_ty(ty);
+                Ok(quote_spanned!(this.span => impl IntoScalar<$t>))
+            }
+        })
+    }
+
+    /// For a Java type
+    fn java_type(&mut self, ty: &Type) -> Result<TokenStream, UnsupportedWildcard> {
         match ty {
             Type::Ref(ty) => self.java_ref_ty(ty),
 
-            Type::Scalar(ty) => match ty {
-                ScalarType::Int => quote_spanned!(self.span => i32),
-                ScalarType::Long => quote_spanned!(self.span => i64),
-                ScalarType::Short => quote_spanned!(self.span => i16),
-                ScalarType::Byte => quote_spanned!(self.span => i8),
-                ScalarType::F64 => quote_spanned!(self.span => f64),
-                ScalarType::F32 => quote_spanned!(self.span => f32),
-                ScalarType::Boolean => quote_spanned!(self.span => bool),
-            },
+            Type::Scalar(ty) => Ok(self.java_scalar_ty(ty)),
         }
     }
 
-    fn java_ref_ty(&mut self, ty: &RefType) -> TokenStream {
+    fn java_ref_ty(&mut self, ty: &RefType) -> Result<TokenStream, UnsupportedWildcard> {
         match ty {
-            RefType::Class(ty) => self.class_ref_ty(ty),
+            RefType::Class(ty) => Ok(self.class_ref_ty(ty)?),
             RefType::Array(e) => {
-                let e = self.java_ref_ty(e);
-                quote_spanned!(self.span => java::JavaArray<#e>)
+                let e = self.java_ref_ty(e)?;
+                Ok(quote_spanned!(self.span => java::JavaArray<#e>))
             }
             RefType::TypeParameter(t) => {
                 let ident = self.java_type_parameter_ident(t);
-                quote_spanned!(self.span => #ident)
+                Ok(quote_spanned!(self.span => #ident))
             }
             RefType::Extends(ty) => {
-                let e = self.java_ref_ty(ty);
-                let g = self.fresh_generic();
+                let g = self.fresh_generic()?;
+                let e = self.java_ref_ty(ty)?;
                 self.push_where_bound(quote_spanned!(self.span => #g : AsRef<#e>));
-                quote_spanned!(self.span => #g)
+                Ok(quote_spanned!(self.span => #g))
             }
             RefType::Super(_) => {
-                let g = self.fresh_generic();
+                let g = self.fresh_generic()?;
                 // FIXME: missing where bound, really
-                quote_spanned!(self.span => #g)
+                Ok(quote_spanned!(self.span => #g))
             }
             RefType::Wildcard => {
-                let g = self.fresh_generic();
-                quote_spanned!(self.span => #g)
+                let g = self.fresh_generic()?;
+                Ok(quote_spanned!(self.span => #g))
             }
         }
     }
 
-    fn class_ref_ty(&mut self, ty: &ClassRef) -> TokenStream {
+    fn class_ref_ty(&mut self, ty: &ClassRef) -> Result<TokenStream, UnsupportedWildcard> {
         let ClassRef { name, generics } = ty;
         let rust_name = name.to_module_name(self.span);
         if generics.len() == 0 {
-            quote_spanned!(self.span => #rust_name)
+            Ok(quote_spanned!(self.span => #rust_name))
         } else {
-            let rust_tys: Vec<_> = generics.iter().map(|t| self.java_ref_ty(t)).collect();
-            quote_spanned!(self.span => #rust_name < #(#rust_tys),* >)
+            let rust_tys: Vec<_> = generics
+                .iter()
+                .map(|t| self.java_ref_ty(t))
+                .collect::<Result<_, _>>()?;
+            Ok(quote_spanned!(self.span => #rust_name < #(#rust_tys),* >))
         }
     }
 
     fn java_type_parameter_ident(&self, t: &Id) -> Ident {
         Ident::new(&format!("J{}", t), self.span)
+    }
+
+    fn java_scalar_ty(&self, ty: &ScalarType) -> TokenStream {
+        match ty {
+            ScalarType::Int => quote_spanned!(self.span => i32),
+            ScalarType::Long => quote_spanned!(self.span => i64),
+            ScalarType::Short => quote_spanned!(self.span => i16),
+            ScalarType::Byte => quote_spanned!(self.span => i8),
+            ScalarType::F64 => quote_spanned!(self.span => f64),
+            ScalarType::F32 => quote_spanned!(self.span => f32),
+            ScalarType::Boolean => quote_spanned!(self.span => bool),
+        }
     }
 }
 
