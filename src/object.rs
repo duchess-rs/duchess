31ab64f490877d539a9bceb81e9c15c5aba1722f
Niko Matsakis <niko@alum.mit.edu>
2023-04-11 07:45:44 -0400

    only use crate as duchess when not in doctest mode

--- src/object.rs
+++ src/object.rs
@@ -1,4 +1,8 @@
+// Make current crate available as `duchess` for use by the generated code.
+// NB. in documentation mode, the current crate is already available as duchess.
+#[cfg(not(doc))]
 use crate as duchess;
+
 use crate::java;
 
 duchess_macro::duchess_javap! {
