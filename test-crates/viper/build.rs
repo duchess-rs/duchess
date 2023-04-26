038e2836359377c1eb3123ea806939c497ef7a1d
Federico Poli <federpoli@gmail.com>
2023-04-26 13:39:07 +0200

    Re-export jni and once_cell

--- test-crates/viper/build.rs
+++ test-crates/viper/build.rs
@@ -1,5 +1,4 @@
 use std::{env, io, fs, path};
-use zip_extensions::zip_extract;
 
 static JAR_URL: &str = "https://github.com/viperproject/viperserver/releases/download/v.23.01-release/viperserver.jar";
 
@@ -7,14 +6,11 @@ fn main() {
     let out_dir_string = env::var("OUT_DIR").unwrap();
     let out_dir = path::Path::new(&out_dir_string);
     let jar_path = out_dir.join("viper.jar");
-    let class_path = out_dir.join("classes");
 
     let jar_data = ureq::get(JAR_URL).call().unwrap();
     let mut jar_file = fs::File::create(jar_path.clone()).unwrap();
     io::copy(&mut jar_data.into_reader(), &mut jar_file).unwrap();
-    zip_extract(&jar_path, &class_path).unwrap();
 
     println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-changed={}", class_path.display());
-    println!("cargo:rustc-env=CLASSPATH={}", class_path.display());
+    println!("cargo:rustc-env=CLASSPATH={}", jar_path.display());
 }
