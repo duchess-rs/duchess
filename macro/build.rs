5c71f7001d0723268a26be87c476a891ce3125d3
Federico Poli <federpoli@gmail.com>
2023-04-27 12:39:59 +0200

    Recompile macro when the grammar changes

--- macro/build.rs
+++ macro/build.rs
@@ -1,6 +1,9 @@
 fn main() {
     lalrpop::process_root().unwrap();
 
+    // Recompile the crate when the lalrpop grammar changes
+    println!("cargo:rerun-if-changed=src");
+
     // Procedural macros currently do not automatically rerun when the environment variables on
     // which they depend change. So, as a workaround we force a recompilation.
     // See issue: https://github.com/duchess-rs/duchess/issues/7
