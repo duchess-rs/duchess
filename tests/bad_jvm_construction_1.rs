b234ce447b87237e683b24728130fca9a8da31a1
Chase Kernan <ckkernan@amazon.com>
2023-05-08 09:16:08 -0700

    Small clean up

--- tests/bad_jvm_construction_1.rs
+++ tests/bad_jvm_construction_1.rs
@@ -2,7 +2,6 @@ use duchess::Jvm;
 
 #[test]
 fn test_jvm_construction_error() {
-    eprintln!("using default");
     Jvm::with(|_jvm| Ok(())).unwrap();
     let res = Jvm::builder().try_launch();
     assert!(matches!(res, Err(duchess::Error::JvmAlreadyExists)));
