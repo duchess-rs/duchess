de791c582516804442d828d1f783da6593806f94
Oli Scherer <git-spam-no-reply9815368754983@oli-obk.de>
2023-05-19 13:05:09 +0000

    Make `//@run` actually work

--- tests/ui.rs
+++ tests/ui.rs
@@ -11,6 +11,10 @@ fn main() -> color_eyre::eyre::Result<()> {
         config.output_conflict_handling = OutputConflictHandling::Bless;
     }
 
+    // Place the build artifacts in the `target/ui` directory instead of in the
+    // crate root.
+    config.out_dir = Some("target/ui".into());
+
     // Make sure we can depend on duchess itself in our tests
     config.dependencies_crate_manifest_path = Some("Cargo.toml".into());
 
