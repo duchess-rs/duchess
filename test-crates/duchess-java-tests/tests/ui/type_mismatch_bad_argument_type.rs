aaf62b1f253c19e0a72d13540910c3436edc744e
Russell Cohen <rcoh@amazon.com>
2024-05-16 11:45:50 -0400

    update tests for new rustc version

--- test-crates/duchess-java-tests/tests/ui/type_mismatch_bad_argument_type.rs
+++ test-crates/duchess-java-tests/tests/ui/type_mismatch_bad_argument_type.rs
@@ -9,12 +9,14 @@ duchess::java_package! {
 
 fn call_with_u64(i: u64) {
     type_mismatch::TakesInt::new().take(i).execute();
-    //~^ ERROR: `u64: duchess::JvmOp` is not satisfied
+    //~^ERROR: the trait bound `u64: IntoScalar<i32>` is not satisfied
+    //~|ERROR: the trait bound `u64: duchess::JvmOp` is not satisfied
 }
 
 fn call_with_u32(i: u32) {
     type_mismatch::TakesInt::new().take(i).execute();
-    //~^ ERROR: `u32: duchess::JvmOp` is not satisfied
+    //~^ ERROR: the trait bound `u32: duchess::JvmOp` is not satisfied
+    //~| ERROR: the trait bound `u32: IntoScalar<i32>` is not satisfied
 }
 
 fn call_with_i32(i: i32) {
