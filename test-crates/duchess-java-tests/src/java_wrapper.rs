a2b70d141f91ec2908559aa1c9332f67276bb6dc
Michael Bower <bowemi@amazon.com>
2024-06-26 07:22:16 -0700

    Update java_wrapper to be OS agnostic

--- test-crates/duchess-java-tests/src/java_wrapper.rs
+++ test-crates/duchess-java-tests/src/java_wrapper.rs
@@ -9,6 +9,7 @@
 //
 // This wrapper script converts the given path into the expected java format
 use std::env;
+use std::ffi::OsStr;
 use std::path::Path;
 use std::process::Command;
 
@@ -18,14 +19,15 @@ fn main() {
 
     let path_to_java_file = args.pop().unwrap();
 
-    println!("path_to_java_file: {}", path_to_java_file);
-
     let path_converted_to_package_fmt = Path::new(&path_to_java_file)
         .strip_prefix("tests/java_ui")
         .unwrap()
         .with_extension("")
-        .to_string_lossy()
-        .replace("/", ".");
+        .iter()
+        .map(OsStr::to_str)
+        .map(Option::unwrap)
+        .collect::<Vec<&str>>()
+        .join(".");
 
     args.push(path_converted_to_package_fmt);
 
@@ -33,11 +35,9 @@ fn main() {
 
     if !output.status.success() {
         let mut stdout: String =
-            String::from_utf8(output.stdout).expect("Unable to parse javac output");
+            String::from_utf8(output.stdout).expect("Unable to parse java output");
         stdout.push_str(String::from_utf8(output.stderr).unwrap().as_str());
 
         panic!("Failed to run java {:?}: {}", args, stdout);
     }
-
-    println!("Tests will fail to compile if this file doesn't exist");
 }
